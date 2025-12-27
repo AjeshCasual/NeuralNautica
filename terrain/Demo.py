72f3cf5feca551205d1d77a2b6e5ae5325c3fe03
AjeshCasual <AjeshCasual@outlook.com>
2025-12-27 23:12:38 +0530

    added hyperparameters

--- terrain/Demo.py
+++ terrain/Demo.py
@@ -3,20 +3,24 @@ import trimesh
 from noise import pnoise3
 from skimage import measure
 
-size = 32
-grid = np.zeros((size, size, size), dtype=np.float32)
+SIZE = 32
+SCALE = 50
+THRESHOLD = 0.1
+OUTPUT_FILE = "out.obj"
 
-for x in range(size):
-    for y in range(size):
-        for z in range(size):
-            grid[x, y, z] = pnoise3(x / 50, y / 50, z / 50)
+grid = np.zeros((SIZE, SIZE, SIZE), dtype=np.float32)
+
+for x in range(SIZE):
+    for y in range(SIZE):
+        for z in range(SIZE):
+            grid[x, y, z] = pnoise3(x / SCALE, y / SCALE, z / SCALE)
 
 
-verts, faces, normals, values = measure.marching_cubes(grid, level=0.3)
+verts, faces, normals, values = measure.marching_cubes(grid, level=THRESHOLD)
 
 mesh = trimesh.Trimesh(vertices=verts, faces=faces)
 
-mesh.export("out.obj")
+mesh.export(OUTPUT_FILE)
 
 mc_mesh = mesh
 
