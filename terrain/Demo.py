9fd569bb37c7a7d61a4c588f3a8e43d9a8874124
AjeshCasual <AjeshCasual@outlook.com>
2025-12-27 23:13:08 +0530

    removed not needed

--- terrain/Demo.py
+++ terrain/Demo.py
@@ -21,21 +21,3 @@ verts, faces, normals, values = measure.marching_cubes(grid, level=THRESHOLD)
 mesh = trimesh.Trimesh(vertices=verts, faces=faces)
 
 mesh.export(OUTPUT_FILE)
-
-mc_mesh = mesh
-
-bounds = mc_mesh.bounds  # [[minx, miny, minz], [maxx, maxy, maxz]]
-
-padding = 5.0  # adjust as needed
-min_corner = bounds[0] - padding
-max_corner = bounds[1] + padding
-
-box_size = max_corner - min_corner
-box_center = (max_corner + min_corner) / 2.0
-outer_box = trimesh.creation.box(
-    extents=box_size, transform=trimesh.transformations.translation_matrix(box_center)
-)
-
-combined = mc_mesh + outer_box
-
-combined.export("out_with_box.obj")
